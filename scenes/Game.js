75d5f16fa305a65aec06c2897e731ff48db4e6be
Juan Martin Vianco <samsungy201533@gmail.com>
2025-06-10 17:26:19 -0300

    agrego la carpeta al repo

---
+++ scenes/Game.js
@@ -0,0 +1,24 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+
+  init() {
+  
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+
+  }
+
+update() {
+
+}
+
+}
